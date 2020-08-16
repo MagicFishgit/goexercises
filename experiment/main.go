5c5c002da126343531237373da57b3d0f1e7bbf6
MagicFishgit <rudivisagie@icloud.com>
2020-08-16 22:06:40 +0200

    lastcommit i think

--- experiment/main.go
+++ experiment/main.go
@@ -1,13 +1,21 @@
 package main
 
+import "fmt"
+
 //functions
 
 func testfunc(t string, t2 int) {
 	println(t, t2)
 }
 
+func returnfunc(s string) string {
+	return fmt.Sprint("Hello ", s)
+}
+
 func main() {
 	testfunc("test", 2)
+	s := returnfunc("Rudi")
+	println(s)
 }
 
 //testing functions
