4c5114a7bfb6cdae6be5c85e222808a58ea7b7c9
MagicFishgit <rudivisagie@icloud.com>
2020-08-29 23:00:16 +0200

    updated comments

--- NinjaLevel11/ex5/dividefunct_test.go
+++ NinjaLevel11/ex5/dividefunct_test.go
@@ -12,4 +12,4 @@ func TestDivideByZero(t *testing.T) {
 	//fmt.Println((100 / 0))
 }
 
-// yeah this is not working out but hey I tried like the exercise wanted me to.
+// yeah this is not working out but hey I tried like the exercise wanted me to. It does give me an expected fail flag but I should have possibly given a better example.
