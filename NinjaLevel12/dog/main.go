c25b18571b0b1971aebdb86375d4c366fa644b68
MagicFishgit <rudivisagie@icloud.com>
2020-08-29 23:47:26 +0200

    added exercise1

---
+++ NinjaLevel12/dog/main.go
@@ -0,0 +1,7 @@
+// Package dog contains functions to help us understand dogs.
+package dog
+
+// ToDogYears takes a Int value and multiplies it by a factor of 7 and then returns the value.
+func ToDogYears(ageH int) int {
+	return ageH * 7
+}
---
+++ NinjaLevel12/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"rudigit/goexercises/NinjaLevel12/dog"
+)
+
+func main() {
+	fmt.Println(dog.ToDogYears(10))
+}
