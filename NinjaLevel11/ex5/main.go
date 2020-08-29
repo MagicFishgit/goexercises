6ddc352ec4cbcb141d117ad5a1b75faec1ff409d
MagicFishgit <rudivisagie@icloud.com>
2020-08-29 23:09:49 +0200

    added some testing of go doc

--- NinjaLevel11/ex5/main.go
+++ NinjaLevel11/ex5/main.go
@@ -5,11 +5,12 @@ import "fmt"
 func main() {
 	//divide by x
 
-	fmt.Println(divide(100, 0))
+	fmt.Println(Divide(100, 0))
 
 }
 
-func divide(x, y float64) float64 {
+//Divide testing go doc
+func Divide(x, y float64) float64 {
 	return (x / y)
 }
 
